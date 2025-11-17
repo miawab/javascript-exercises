c1baf69a92c45b8070c38bd52771add37abac18f
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-17 15:37:32 +0500

    Task 05

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let string2 = "";
+    if (num >= 0 ){
+        for(let i = 0;i<num;i++){
+            string2= string2 + string
+        }
+        return string2;
+    }
+    else{
+        return "ERROR";
+    }
 
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
