32514b6ac687d174f83cc2f3f6d7b7edc8185864
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2025-11-17 22:04:10 +0500

    Task 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!(a === Number(a) && b === Number(b)))
+        return "ERROR";
+    if( a<0 || b<0 || a%1!== 0 || b%1!== 0)
+        return "ERROR";
+    let sum;
+    if (a>b){
+        sum = a;
+        a = b;
+        b = sum;
+    }
+    sum = 0;
+    for(a;a<=b;a++){
+        sum = sum + a;
+    }
+    return sum;
 };
 
 // Do not edit below this line
