30dffd126b5c283c0648e05a7005063d8b1c9d91
Ibrahim awab <179867549+miawab@users.noreply.github.com>
2026-01-12 00:07:19 +0500

    14 completed

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if(x<=1){
+        return x;
+    }
+    return fibonacci(x-1) + fibonacci(x-2)
 };
 
 // Do not edit below this line
